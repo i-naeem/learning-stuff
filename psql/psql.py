09fc686c608b736d8fdf199052807e542ae51635
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-03 20:38:00 +0500

    :construction: adding `fake_users`

--- psql/psql.py
+++ psql/psql.py
@@ -1,14 +1,22 @@
 import sqlite3
+import json
 
 connection = sqlite3.connect(':memory:')
 cursor = connection.cursor()
 
 cursor.execute(
-    """CREATE TABLE users_table(
+    """CREATE TABLE users(
         user_id text
         user_name text
-        last_name text
         first_name text
+        last_name text
+        email text
+        gender text
     )
     """
 )
+
+
+with open(file='fake_users.json', mode='r', encoding='utf-8') as _file:
+    for user in json.load(_file):
+        cursor.execute('INSERT INTO users VALUES (?)')
