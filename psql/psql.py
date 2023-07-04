1849c98654dd29d48357e9d6a636b2fc38587b06
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-04 09:48:09 +0500

    populate fake users to database

--- psql/psql.py
+++ psql/psql.py
@@ -6,17 +6,19 @@ cursor = connection.cursor()
 
 cursor.execute(
     """CREATE TABLE users(
-        user_id text
-        user_name text
-        first_name text
-        last_name text
-        email text
+        first_name text,
+        user_name text,
+        last_name text,
+        user_id text,
+        email text,
         gender text
     )
     """
 )
 
-
+# Populating database with fake users.
 with open(file='fake_users.json', mode='r', encoding='utf-8') as _file:
-    for user in json.load(_file):
-        cursor.execute('INSERT INTO users VALUES (?)')
+    cursor.executemany(
+        'INSERT INTO users(user_id, user_name, first_name, last_name, email, gender) VALUES (:user_id, :user_name, :first_name, :last_name, :email, :gender)', json.load(_file))
+
+    connection.commit()
