4a79c685b6afbf7a5ffb5b073cee7a4182674123
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-04 17:54:18 +0500

    Update signin form

--- fwtf/app.py
+++ fwtf/app.py
@@ -1,11 +1,13 @@
 from flask import render_template
 from forms.Signin import Signin
+from flask import redirect
+from flask import url_for
+from flask import flash
 from flask import Flask
-import os
 
 app = Flask(__name__, template_folder='views')
 
-app.secret_key = os.environ['SECRET_KEY']
+app.secret_key = "8115f854ce4f72a249a573e3e122abffe85511915b5bb76988287c932956cbdb"
 
 
 @app.get('/')
@@ -13,9 +15,14 @@ def index():
     return render_template('index.jinja', title="Home")
 
 
-@app.get('/signin')
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
     form = Signin()
+
+    if form.validate_on_submit():
+        flash(f'You are logged in as {form.username.data}!')
+        return redirect(url_for('index'))
+
     return render_template('signin.jinja', title="Sign in", form=form)
 
 
--- fwtf/forms/Signin.py
+++ fwtf/forms/Signin.py
@@ -2,11 +2,10 @@ from wtforms.validators import DataRequired
 from wtforms import PasswordField
 from flask_wtf import FlaskForm
 from wtforms import StringField
+from wtforms import SubmitField
 
 
 class Signin(FlaskForm):
-    username = StringField(
-        label="Username",
-        validators=[DataRequired()],
-    )
+    username = StringField(label="Username", validators=[DataRequired()])
     password = PasswordField(label="Password", validators=[DataRequired()])
+    submit = SubmitField('Sign in')
