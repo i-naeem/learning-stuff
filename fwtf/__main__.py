2f43c7ce05822f1df40dcd7c49bc644ff782fe11
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-04 11:20:16 +0500

    Add templates

--- fwtf/__main__.py
+++ fwtf/__main__.py
@@ -4,19 +4,19 @@ from flask import Flask
 app = Flask(__name__, template_folder='views')
 
 
+@app.get('/')
+def index():
+    return render_template('index.jinja', title="Home")
+
+
 @app.get('/signin')
 def signin():
-    return 'Login'
+    return render_template('signin.jinja', title="Sign in")
 
 
 @app.get('/signup')
 def signup():
-    return 'Sign up'
-
-
-@app.get('/')
-def index():
-    return render_template('index.jinja')
+    return render_template('signup.jinja', title="Sign up")
 
 
 if __name__ == '__main__':
