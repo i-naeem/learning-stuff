39835cc2254adfce753239ba22c941955d6971b4
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-10 13:16:49 +0500

    add other routes

--- react-router-contact-manager/src/main.jsx
+++ react-router-contact-manager/src/main.jsx
@@ -1,10 +1,30 @@
-import App from './App.jsx';
+import './index.css';
 import React from 'react';
-import Root from './routes/root.jsx';
+import Root, { loader as rootLoader, action as rootAction } from './routes/root.jsx';
 import ReactDOM from 'react-dom/client';
+import ErrorPage from './error-page.jsx';
+import Contact, { loader as contactLoader } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([{ path: '/', element: <Root /> }]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    loader: rootLoader,
+    action: rootAction,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/contacts/:contactId', loader: contactLoader, element: <Contact /> },
+      {
+        path: '/contacts/:contactId/edit',
+        action: editAction,
+        loader: contactLoader,
+        element: <EditContact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
