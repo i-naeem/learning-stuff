bddab16d89876bc4081120a24d366051926da6b3
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-10 12:50:34 +0500

    setup router

--- react-router-contact-manager/src/main.jsx
+++ react-router-contact-manager/src/main.jsx
@@ -1,9 +1,13 @@
 import App from './App.jsx';
 import React from 'react';
+import Root from './routes/root.jsx';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([{ path: '/', element: <Root /> }]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
