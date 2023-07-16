a3fd422f3c0e5819717db74824783f749851c94f
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-16 12:00:47 +0500

    fix: fixed `kwargs` after `args`

--- fluentpy/dublincore.py
+++ fluentpy/dublincore.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
-from datetime import datetime
 from typing import Optional
 from enum import Enum, auto
+from datetime import date
 
 
 class ResourceType(Enum):
@@ -12,11 +12,20 @@ class ResourceType(Enum):
 
 @dataclass
 class Resource:
+    identifier: str
+    creators: str
+    title: str
+
     subjects: list[str] = field(default_factory=list)
     type: ResourceType = ResourceType.BOOK
     date: Optional[date] = None
     description: str = ''
     language: str = ''
-    identifier: str
-    creators: str
-    title: str
+
+
+description = 'Improving the design of existing code'
+book = Resource('978-0-13-475759-9', 'Refactoring, 2nd Edition',
+                ['Martin Fowler', 'Kent Beck'], date(2018, 11, 19),
+                ResourceType.BOOK, description, 'EN',
+                ['computer programming', 'OOP']
+                )
