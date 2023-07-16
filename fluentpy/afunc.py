09b121e2d08274ee5f6b5f3692ea5442578f17ca
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-16 12:12:49 +0500

    create method callers `methodcaller`

--- fluentpy/afunc.py
+++ fluentpy/afunc.py
@@ -1,6 +1,21 @@
 # Positional Arguments Only Function
+from operator import methodcaller
+
+
 def positional_only(a, b, /, c=1):
     print(a, b, c)
 
 
 positional_only(1, 2, c=2)
+
+
+hyphenate = methodcaller('replace', ' ', '_')
+to_lowercase = methodcaller('lower')
+# joiner = methodcaller('join', ' ')
+
+
+dt = ['Audrey McCullen', 'Mark Wood', 'Steve Smith']
+
+
+for n in dt:
+    print(to_lowercase(hyphenate(n)))
