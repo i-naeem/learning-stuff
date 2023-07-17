16e3d16dc3136a3ef954de5a6ff39107f8d2eeaa
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-17 18:56:14 +0500

    create a simple spinner to show `threading`

---
+++ fluentpy/spinner_thread.py
@@ -0,0 +1,40 @@
+from threading import Thread, Event
+import itertools
+import time
+
+
+def spin(msg: str, done: Event) -> None:
+    for char in itertools.cycle(r'\|/-'):
+        status = f'\r{char} {msg}'
+        print(status, end='', flush=True)
+        if done.wait(.1):
+            break
+
+    blanks = ' ' * len(status)
+    print(f'\r{blanks}\r', end='')
+
+
+def slow() -> int:
+    time.sleep(3)
+    return 4
+
+
+def supervisor() -> int:
+    done = Event()
+
+    spinner = Thread(target=spin, args=("thinking!", done))
+    print(f'Spinner Object: {spinner}')
+    spinner.start()
+    result = slow()
+    done.set()
+    spinner.join()
+    return result
+
+
+def main() -> None:
+    result = supervisor()
+    print(f'Answer: {result}')
+
+
+if __name__ == '__main__':
+    main()
