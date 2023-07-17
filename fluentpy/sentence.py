649004cecf6ec24fa0bce2b01e80f17b31dd959c
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-17 17:16:00 +0500

    Create sentence class from `Chapter#17`

---
+++ fluentpy/sentence.py
@@ -0,0 +1,23 @@
+import reprlib
+import re
+
+
+RE_WORD = re.compile(r'\w+')
+
+
+class Sentence:
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(self.text)
+
+    def __getitem__(self, index):
+        return self.words[index]
+
+    def __len__(self):
+        return len(self.words)
+
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+
+
+s = Sentence('Hello World')
