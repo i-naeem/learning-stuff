bc190f0d49d709f42dcb2a1e011be68841cdd6a6
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-18 11:05:14 +0500

    asyncio

---
+++ fluentpy/aio.py
@@ -0,0 +1,19 @@
+import asyncio
+import time
+
+
+async def do_work(task_no):
+    print('Doing', task_no, 'Task')
+    await asyncio.sleep(1)
+
+
+async def main():
+    t0 = time.perf_counter()
+    tasks = [asyncio.create_task(do_work(i)) for i in range(1, 6)]
+    done, pending = await asyncio.wait(tasks)
+
+    print('Finished in', time.perf_counter() - t0)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
