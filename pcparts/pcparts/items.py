4108f3be2a09571ef028bf69f7a27a77f43326f4
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-08-24 18:11:56 +0500

    scrape content

--- pcparts/pcparts/items.py
+++ pcparts/pcparts/items.py
@@ -3,10 +3,14 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class PcpartsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class PcpartsItem(Item):
+    title = Field()
+    brand = Field()
+    price = Field()
+    media = Field()
+    rating = Field()
+    in_stock = Field()
+    description = Field()
