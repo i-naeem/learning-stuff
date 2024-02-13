88c3f9973c5dc17cc0326774ed4cfe83f52ef814
Muhammad Naeem <inaeem@protonmail.com>
2024-02-13 15:28:08 +0500

    added local postgresql database

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,4 +1,5 @@
-const { db } = require('@vercel/postgres');
+const { Client } = require('pg');
+
 const {
   invoices,
   customers,
@@ -161,7 +162,43 @@ async function seedRevenue(client) {
 }
 
 async function main() {
-  const client = await db.connect();
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+  });
+
+  await client.connect();
+
+  const values = (values, { columns = Object.keys(values) } = {}) => {
+    if (!Array.isArray(values)) {
+      values = columns.map((column) => values[column]);
+    }
+    return (valuePosition) => ({
+      text: Array.apply(null, { length: values.length })
+        .map(() => '$' + ++valuePosition)
+        .join(', '),
+      values,
+    });
+  };
+  client.sql = (textFragments, ...valueFragments) => {
+    const query = {
+      text: textFragments[0],
+      values: [],
+    };
+    valueFragments.forEach((valueFragment, i) => {
+      if (typeof valueFragment !== 'function') {
+        valueFragment = values([valueFragment]);
+      }
+      valueFragment = valueFragment(query.values.length);
+      query.text += valueFragment.text + textFragments[i + 1];
+      query.values = query.values.concat(valueFragment.values);
+    });
+    return client.query(query.text, query.values);
+  };
 
   await seedUsers(client);
   await seedCustomers(client);
